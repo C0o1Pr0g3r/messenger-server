4e65b1f8b87d7868cd4171ef9c8a4f3f610b5076
Mark <m4rc0d3r@gmail.com>
2025-07-28 15:42:38 +0300

    Configure ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,47 @@
 // @ts-check
 import eslint from "@eslint/js";
+import importPlugin from "eslint-plugin-import";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   {
-    ignores: ["eslint.config.mjs", "prettier.config.mjs"],
+    ignores: ["dist", "eslint.config.mjs", "prettier.config.mjs"],
   },
   eslint.configs.recommended,
+  {
+    rules: {
+      eqeqeq: "error",
+    },
+  },
   ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    rules: {
+      "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+      "@typescript-eslint/consistent-type-exports": "error",
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/explicit-member-accessibility": [
+        "error",
+        {
+          accessibility: "no-public",
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
   eslintPluginPrettierRecommended,
   {
     languageOptions: {
@@ -24,11 +56,40 @@ export default tseslint.config(
       },
     },
   },
+  importPlugin.flatConfigs.recommended,
   {
+    settings: {
+      "import/resolver": {
+        typescript: {
+          alwaysTryTypes: true,
+        },
+        node: {
+          extensions: [".js", ".ts"],
+        },
+      },
+    },
     rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-floating-promises": "warn",
-      "@typescript-eslint/no-unsafe-argument": "warn",
+      "import/no-empty-named-blocks": "error",
+      "import/no-absolute-path": "error",
+      "import/no-self-import": "error",
+      "import/no-useless-path-segments": [
+        "error",
+        {
+          noUselessIndex: true,
+        },
+      ],
+      "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
+      "import/exports-last": "error",
+      "import/first": "error",
+      "import/group-exports": "error",
+      "import/newline-after-import": "error",
+      "import/no-named-default": "error",
+      "import/order": [
+        "error",
+        {
+          "newlines-between": "always",
+        },
+      ],
     },
   },
 );
