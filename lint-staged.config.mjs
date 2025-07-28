430bbe6cf7a11ba92e285c2b871630306bd86f59
Mark <m4rc0d3r@gmail.com>
2025-07-28 16:31:44 +0300

    Add syncpack

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
-import { runEslint, runPrettier } from "./lint-staged/commands.mjs";
+import { runEslint, runPrettier, runSyncpackFormat } from "./lint-staged/commands.mjs";
 import { setUpTasksForTypescriptFiles } from "./lint-staged/helpers.mjs";
 
 /** @type {import("lint-staged").Configuration} */
 export default {
   "*.{json,md,yaml,yml,sql}": [runPrettier()],
   "*.{js,mjs,cjs}": [runEslint(), runPrettier()],
+  "**/package.json": async (files) => [await runSyncpackFormat(files)],
   ...setUpTasksForTypescriptFiles([
     {
       glob: "src/**/*.{ts,mts,cts}",
